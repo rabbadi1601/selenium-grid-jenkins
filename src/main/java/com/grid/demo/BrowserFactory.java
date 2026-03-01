3be9d68d8dc6c0435a0f2ea3bc8179e7140e331a
rabbadi1601 <56824269+rabbadi1601@users.noreply.github.com>
2026-03-01 09:22:23 +0530

    Initial Selenium Grid project

---
+++ src/main/java/com/grid/demo/BrowserFactory.java
@@ -0,0 +1,34 @@
+package com.grid.demo;
+
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver getDriver(String browser) throws Exception {
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            return new RemoteWebDriver(new URL("http://localhost:4444"), options);
+        }
+
+        else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            return new RemoteWebDriver(new URL("http://localhost:4444"), options);
+        }
+
+        else if (browser.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            return new RemoteWebDriver(new URL("http://localhost:4444"), options);
+        }
+
+        else {
+            throw new RuntimeException("Invalid browser");
+        }
+    }
+}
